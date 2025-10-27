cf7b8e32017c7a1ff252c7d0329e01a196435dee
user <test@gmail.com>
2025-10-27 11:07:48 +0200

    velviens neizdevies un nepareizs uzdevums

---
+++ internet-store.js
@@ -0,0 +1,8 @@
+let storage ;
+(  Name = "phone",category = "elektronika", price = "300€", count = "50");
+(  Name = "laptop", category = "elektronika",price = "670€", count = "29");
+(  Name = "shoes",category = "apģērbs", price = "30€", count = "4")
+console.log("1. Kuras preces pieder kategorijai Elektronika?");
+console.log("phone. laptop");
+console.log("2. Kāda ir kopējā visu preču vērtība noliktavā?");
+console.log("34550 ")
